1b40f12d1b6c3d34566e5b91416d75bbbec94749
LucHighwalker <luc.highwalker@gmail.com>
2018-10-28 15:31:22 -0700

    - Optimized stochastic generation.

--- stochastic.py
+++ stochastic.py
@@ -2,13 +2,17 @@ import sys
 import random
 import time
 
-lines = list()
+histogram = {}
+keys = list()
 
 
 def open_file(file):
-    global lines
     lines = [line.rstrip('\n')
              for line in open(file)]
+    for line in lines:
+        word_value = line.split(' ')
+        keys.append(word_value[0])
+        histogram[word_value[0]] = int(word_value[1])
 
 
 def generate_sentence(count):
@@ -17,9 +21,8 @@ def generate_sentence(count):
     iterations = 0
     while len(sentence) < int(count):
         iterations = iterations + 1
-        random_line = random.choice(lines).split(' ')
-        random_word = random_line[0]
-        word_chance = int(random_line[1])
+        random_word = random.choice(keys)
+        word_chance = histogram[random_word]
         rand_chance = random.random() * 4200
         if rand_chance < word_chance:
             sentence.append(random_word)
