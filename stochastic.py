4d35757b923eca17084bddd72569a450c506c47f
LucHighwalker <luc.highwalker@gmail.com>
2018-11-06 20:36:58 -0800

    - Heroku commit.

--- stochastic.py
+++ stochastic.py
@@ -2,28 +2,27 @@ import sys
 import random
 import time
 
-histogram = {}
-keys = list()
 
-
-def open_file(file):
+def open_histogram(file):
+    histogram = {}
     lines = [line.rstrip('\n')
              for line in open(file)]
     for line in lines:
         word_value = line.split(' ')
-        keys.append(word_value[0])
         histogram[word_value[0]] = int(word_value[1])
+    return histogram
 
 
-def generate_sentence(count):
+def generate_sentence(histogram, count):
     start_time = time.time()
+    keys = list(histogram.keys())
     sentence = list()
     iterations = 0
     while len(sentence) < int(count):
         iterations = iterations + 1
         random_word = random.choice(keys)
         word_chance = histogram[random_word]
-        rand_chance = random.random() * 4200
+        rand_chance = random.random() * len(keys)
         if rand_chance < word_chance:
             sentence.append(random_word)
     generation_time = time.time() - start_time
@@ -34,6 +33,6 @@ def generate_sentence(count):
 
 if __name__ == "__main__":
     params = sys.argv[1:]
-    open_file(params[0])
-    sentence = generate_sentence(params[1])
+    histogram = open_histogram(params[0])
+    sentence = generate_sentence(histogram, params[1])
     print(sentence)
